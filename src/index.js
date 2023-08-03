df1bea7f9a86b8dde38380b3b9a58aeb0b3e4211
Faiz Mohammad <email2faizm@gmail.com>
2023-08-04 01:36:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles.css';
 import { addScore, updateScoresList } from './modules/leaderboard';
+const addScoreBtn = document.getElementById('add-score');
+const refreshBtn = document.getElementById('scores-refresh');
+const gameIdDisplay = document.getElementById('game-id-display');
