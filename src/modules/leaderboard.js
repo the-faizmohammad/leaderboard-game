3f087777b691c5de304f1e56482d6c64519aeeef
Faiz Mohammad <email2faizm@gmail.com>
2023-08-04 01:32:47 +0530

    Update updateScoresList in leaderboard.js

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,3 +1,23 @@
 const baseUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
 const gameId = 'e9hgUU8qvHHdchrqFL6R'; 
 export const updateScoresList = async () => {
+  try {
+    const response = await fetch(`${baseUrl}games/${gameId}/scores/`);
+    const responseData = await response.json();
+    const scores = responseData.result;
+
+    const scoresList = document.querySelector('.scores-list');
+    scoresList.innerHTML = '';
+
+    scores.forEach((scoreData) => {
+      const li = document.createElement('li');
+      li.innerHTML = `
+        <p class="country">${scoreData.user}</p>
+        <p class="score-value">${scoreData.score}</p>
+      `;
+      scoresList.appendChild(li);
+    });
+  } catch (error) {
+    console.error('Error getting game scores:', error);
+  }
+};
